aa8a377b409af8c2c277e643352c9c72f93fb618
SolomahinMaxim <nogiubogi0@gmail.com>
2021-04-11 13:17:19 +0300

    Create footer.php

---
+++ blocks/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div id="social">
+        <a href="https://vk.com/wormandflea"><img src="../image/vk.png" alt="Максим Ракушкин"></a>Максим Соломахин &copy;<?php echo ' ' . date('Y')?>
+    </div>
+</footer>
