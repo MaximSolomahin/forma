4955221999a9b65868c42f2f12667333c5eb204f
SolomahinMaxim <nogiubogi0@gmail.com>
2021-04-11 13:17:21 +0300

    Create head.php

---
+++ blocks/head.php
@@ -0,0 +1,9 @@
+<?php
+    require_once "../functions/functions.php";
+?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, inital-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" type="text/css" href="/css/style.css">
+
+<title><?php echo $title ?></title>
