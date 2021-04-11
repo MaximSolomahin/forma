527b6d31a48a9b886779f7a36e63edc0be2ca8b5
SolomahinMaxim <nogiubogi0@gmail.com>
2021-04-11 20:09:39 +0300

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -1,26 +1,48 @@
 <?php
-
 function getNews($num)
 {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
+    $link = getConnect();
+    mysqli_query($link, "SET NAMES 'UTF-8'");
+
+    $table = 'news';
+    $query = "SELECT * FROM $table WHERE id > 0 LIMIT $num";
+
+    $result = mysqli_query($link, $query);
+
+    for ($date = []; $row = mysqli_fetch_assoc($result); $date[] = $row) ;
+    mysqli_close($link);
+    return $date;
+}
+
+function getOneNews($id){
+    $link = getConnect();
+    mysqli_query($link, "SET NAMES 'UTF-8'");
+    $table = 'news';
+    $query = "SELECT * FROM $table WHERE id = $id";
+
+    $result = mysqli_query($link, $query);
+
+    for ($date = []; $row = mysqli_fetch_assoc($result); $date[] = $row) ;
+    mysqli_close($link);
+    return $date;
+
+
+}
+
+function getConnect(){
     $host = 'localhost';
     $login = 'root';
     $password = 'root';
     $db_name = 'news_db';
 
     $link = mysqli_connect($host, $login, $password, $db_name);
-    mysqli_query($link, "SET NAMES 'UTF-8'");
 
-    $table = 'news';
-    $query = "SELECT * FROM $table WHERE id > 0 LIMIT $num";  ///// Вся работа делалась тут: Вначале идет работа со столбцами. А в конце работа с данными и их поиск.
-
-    $result = mysqli_query($link, $query);
-
-    for ($date = []; $row = mysqli_fetch_assoc($result); $date[] = $row) ;
-    return $date;
+    return $link;
 }
+
 
 
 
