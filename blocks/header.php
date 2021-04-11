a9deabc295cd8269f398a19b37b60465b9701525
SolomahinMaxim <nogiubogi0@gmail.com>
2021-04-11 13:17:23 +0300

    Create header.php

---
+++ blocks/header.php
@@ -0,0 +1,12 @@
+
+<body>
+<header>
+    <div>
+        <div id="logo">
+            <a href="/" title="Перейти на главную"><span>Главная|обратная связь</span></a>
+        </div>
+        <div id="news">
+            <a href="/news/news.php"><span>Новости</span></a>
+        </div>
+    </div>
+</header>
