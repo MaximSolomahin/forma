22569e6e5d9c0c5312bca04b39f8b46bfe5b0f78
SolomahinMaxim <nogiubogi0@gmail.com>
2021-04-11 20:09:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,16 +12,16 @@
     include_once 'blocks/header.php';
 ?>
 <?php if (!isset($_REQUEST['submit'])){?>
-        <div id="form" >
-            <form action="" method="POST">
-                <input type="text" name="name"  placeholder="Введите ваше имя" required pattern="[a-zA-Zа-яА-ЯёЁ]+">
-                <input type="text" name="address" placeholder="Введите ваш адрес" required>
-                <input type="text" name="number" placeholder="Введите ваш номер телефона" required pattern="\+?[0-9]{11,12}" ">
-                <input type="email" name="email" placeholder="Введите ваш e-mail" pattern="^[0-9a-zA-Z-.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$" >
+        <div id="main">
+             <div id="form" >
+                 <form action="" method="POST">
+                    <input type="text" name="name"  placeholder="Введите ваше имя" required pattern="[a-zA-Zа-яА-ЯёЁ\s]+">
+                    <input type="text" name="address" placeholder="Введите ваш адрес" required>
+                    <input type="text" name="number" placeholder="Введите ваш номер телефона" required pattern="\+?[0-9]{11,12}" ">
+                    <input type="email" name="email" placeholder="Введите ваш e-mail" pattern="^[0-9a-zA-Z-.]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$" >
 
-                <input id="button" type="submit" name="submit">
-
-        </form>
+                    <input id="button" type="submit" name="submit">
+                </form>
     </div>
     <?php
 } else {
@@ -52,19 +52,23 @@
     else {
         $date = [$name, $address, $number, $email];
     ?>
-    <table class="table">
-        <tr>
-            <th>Имя</th>
-            <th>Адрес</th>
-            <th>Номер телефона</th>
-            <th>E-mail</th>
-        </tr>
-        <tr>
-            <?php foreach ($date as $elem){
-                echo"<th>$elem</th><br>";
-            }?>
-        </tr>
-    </table>
+            <div id="table">
+                <table>
+                    <tr>
+                        <th>Имя</th>
+                        <th>Адрес</th>
+                        <th>Номер телефона</th>
+                        <th>E-mail</th>
+                    </tr>
+                    <tr>
+                        <?php foreach ($date as $elem){
+                            echo"<th>$elem</th><br>";
+                        }?>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
     <?php
 
         }
