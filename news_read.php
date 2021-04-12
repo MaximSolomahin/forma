09f1c949d24e5703e0aadbba6605af7c50503932
SolomahinMaxim <nogiubogi0@gmail.com>
2021-04-12 21:16:42 +0300

    Update news_read.php

--- news_read.php
+++ news_read.php
@@ -11,28 +11,21 @@
 <body>
 <?php
 include_once "blocks/header.php";
-echo    '<br>';
+
 $news = getOneNews($_GET['id']);
 $id = $_GET['id'];
-echo    '<br>';
-
-
 
 ?>
 
-   <div id="newsLentFull">
-       <div id="newsFull">
-            <div id="firstNewsFull">
-                 <br><img src="../image/<?=$news[0]['id']?>.jpg" alt="Статья 1" title="<?=$news[0]['id']?>'">
-                                <h2><?=$news[0]['title']?></h2>
-                                <p> <?=$news[0]['full_text']?></p>
+<div id="newsLentFull">
+    <div id="newsFull">
+        <div id="firstNewsFull">
+            <br><img src="../image/<?=$news[0]['id']?>.jpg" alt="Статья 1" title="<?=$news[0]['id']?>'">
+            <h2><?=$news[0]['title']?></h2>
+            <p> <?=$news[0]['full_text']?></p>
         </div>
     </div>
 </div>
-
-
-
-
 
 <?php
 include_once 'blocks/footer.php';
@@ -40,11 +33,4 @@ include_once 'blocks/footer.php';
 
 </body>
 
-</html>
-
-
-<!--echo '<br>' . '<img src="../image/'. $news[0]['id'] .'.jpg" alt="Статья 1" title=". ' . $news[0]['id'] .'">
-
-                                <h2>'. $news[0]['title'] . '</h2>
-                                <p> ' . $news[0]['full_text'] . '</p>
-                            </a> -->
+</html>                          </a> -->
